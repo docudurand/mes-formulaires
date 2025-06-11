01795b02426b6b08d0fcccf81661d9c12986fcdf
docudurand <magvl4gleize@durandservices.fr>
2025-06-11 10:53:39 +0200

    Add files via upload

--- formtelevente/index.js
+++ formtelevente/index.js
@@ -18,8 +18,8 @@ router.get('/healthz', (_req, res) => {
 const salesMap = {
   'Casti Jeremy':   'comvl2miribel@durandservices.fr',
   'Trenti Anthony': 'comvlchassieu@durandservices.fr',
-  'Bazoge Ilona':   'angeliquemanin@hotmail.fr',
-  'Pichard Damien': 'd.pichard2007@gmail.com'
+  'Bazoge Ilona':   'comvl2chassieu@durandservices.fr',
+  'Pichard Damien': 'magvl4gleize@durandservices.fr'
 };
 
 const transporter = nodemailer.createTransport({
