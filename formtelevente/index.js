12795e78b837ac64f5468cb785e757b2dacb6f2c
docudurand <magvl4gleize@durandservices.fr>
2025-08-21 08:28:23 +0200

    Add files via upload

--- formtelevente/index.js
+++ formtelevente/index.js
@@ -18,9 +18,8 @@ const salesMap = {
   'Casti Jeremy':   'comvl2miribel@durandservices.fr,magvl4gleize@durandservices.fr',
   'Trenti Anthony': 'comvlchassieu@durandservices.fr,magvl4gleize@durandservices.fr',
   'Bazoge Ilona':   'comvl2chassieu@durandservices.fr,magvl4gleize@durandservices.fr',
-  'Vigne Gregory':   'gvigne@plateformepavi.fr,magvl4gleize@durandservices.fr',
+  'Barret Olivier': 'comvlmiribel@durandservices.fr',
   'Merolle Nicolas':   'nmerolle@durandservices.fr,magvl4gleize@durandservices.fr',
-  'Roberjot Stéphanie':   'sroberjot@plateformepavi.fr,magvl4gleize@durandservices.fr',
   'Pichard Damien': 'magvl4gleize@durandservices.fr'
 };
 
