6a77ebb93bea7b0d2a555030bea5e37e07120b57
docudurand <magvl4gleize@durandservices.fr>
2025-09-04 13:08:01 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
