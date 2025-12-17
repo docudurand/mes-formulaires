f46a67a8ae562212d13aee173d4de76ee07279c8
docudurand <magvl4gleize@durandservices.fr>
2025-12-17 21:56:14 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -34,6 +34,15 @@ const __dirname  = path.dirname(__filename);
 const app = express();
 app.set("trust proxy", 1);
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "frame-ancestors 'self' https://documentsdurand.wixsite.com https://*.wixsite.com https://*.wix.com https://*.editorx.io https://*.onrender.com"
+  );
+  res.removeHeader("X-Frame-Options");
+
+  next();
+});
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
