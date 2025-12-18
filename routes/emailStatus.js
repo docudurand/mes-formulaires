db9d03ef60b2867010692c155ddd947fc2cbcd01
docudurand <magvl4gleize@durandservices.fr>
2025-12-18 19:06:53 +0100

    Delete routes/emailStatus.js

--- routes/emailStatus.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { listStatus } from "../dataStore.js";
-const router = express.Router();
-
-router.get("/email-status", (_req,res)=>{
-  res.json(listStatus());
-});
-
-export default router;
