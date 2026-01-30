98f9bfa8dfb2425e5ff7941d06fb22a3d0e60494
damien pichard <magvl4gleize@durandservices.fr>
2026-01-30 23:47:10 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ import presences from "./routes/presences.js";
 import ramasseRouter from "./routes/ramasse.js";
 import kilometrageRouter from "./routes/kilometrage.js";
 import "./mailInlineWorker.js";
-import garantieRouter from "./garantie/router.js";
 
 // Chargement des variables d'environnement
 dotenv.config();
@@ -172,13 +171,6 @@ app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 // Routes pour lecture des logs mails
 app.use(mailLogsRouter);
-
-// Sert les pages Garantie en statique
-app.use("/garantie", express.static(path.join(__dirname, "garantie"), {
-  extensions: ["html", "htm"],
-  index: false
-}));
-
 
 // bloque si une variable d'environnement manque
 function mustEnv(name) {
@@ -2019,7 +2011,6 @@ ts: new Date().toISOString(),
 app.get("/", (_req, res) => res.status(200).send("📝 Mes Formulaires – service opérationnel"));
 
 // Routes des differents formulaires
-app.use("/garantie/api", garantieRouter);
 app.use("/formtelevente", formtelevente);
 app.use("/formulaire-piece", formulairePiece);
 app.use("/formulaire-piecepl", formulairePiecePL);
